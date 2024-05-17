7ebdb7d27289a830a598398973234ba379d8d3fa
Yash Sehgal <yash@thexyzstudio.com>
2024-05-17 13:35:49 +0530

    about us responsiveness done

--- src/app/websitedevelopment/components/about/index.tsx
+++ src/app/websitedevelopment/components/about/index.tsx
@@ -8,57 +8,85 @@ import BgCircles from '@/components/organisms/bgCircles';
 
 const About = () => {
   return (
-    <div className="flex-1 overflow-hidden relative md:py-10 md:pl-10 md:mx-20 bg-dark rounded-4xl">
+    <div className="flex-1 overflow-hidden relative py-8 pl-4 pr-4 sm:pr-4 md:pr-0 sm:py-10 sm:pl-10 mx-6 sm:mx-20 bg-dark rounded-4xl">
       <div className="flex flex-row justify-center  items-center">
         <p
-          className={`text-orange ${chau_philomene.className} font-bold leading-44px text-4xl`}
+          className={`text-orange ${chau_philomene.className} text-xl sm:text-3xl md:text-4xl leading-7 sm:leading-10 md:leading-44px `}
         >
           About Us
         </p>
-        <svg
-          className="md:mx-6"
-          xmlns="http://www.w3.org/2000/svg"
-          width="4"
-          height="102"
-          viewBox="0 0 4 102"
-          fill="none"
-        >
-          <path
-            d="M2 2V100"
-            stroke="#F17C04"
-            stroke-width="4"
-            stroke-linecap="round"
-          />
-        </svg>
+        <div className="block sm:hidden">
+          <svg
+            className="mx-3 sm-20 md:mx-6"
+            xmlns="http://www.w3.org/2000/svg"
+            width="2"
+            height="42"
+            viewBox="0 0 2 42"
+            fill="none"
+          >
+            <path
+              d="M1 1V41"
+              stroke="#F17C04"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+          </svg>
+        </div>
+        <div className="hidden sm:block">
+          <svg
+            className="mx-3 sm-20 md:mx-6"
+            xmlns="http://www.w3.org/2000/svg"
+            width="4"
+            height="102"
+            viewBox="0 0 4 102"
+            fill="none"
+          >
+            <path
+              d="M2 2V100"
+              stroke="#F17C04"
+              stroke-width="4"
+              stroke-linecap="round"
+            />
+          </svg>
+        </div>
         <div>
-          <p className="text-white  leading-38px text-3xl font-medium">
+          <p className="text-white text-xs leading-5 sm:text-2xl sm:leading-8  md:leading-38px md:text-3xl font-medium">
             Cosmic Companion in the
           </p>
-          <p className="text-white  leading-38px text-3xl font-medium">
+          <p className="text-white text-xs leading-5 sm:text-2xl sm:leading-8  md:leading-38px md:text-3xl font-medium">
             Digital Odyssey
           </p>
         </div>
       </div>
-      <div className="md:mt-29px items-center justify-between flex flex-row">
-        <div className="w-6/12">
-          <p className="text-white text-2xl leading-36px font-regular">
+      <div className="mt-10 sm:mt-[30px] items-center justify-between flex flex-row">
+        <div className="w-full  sm:w-6/12">
+          <p className="text-white text-xs leading-5 sm:text-base  sm:leading-6  md:text-2xl md:leading-36px font-regular">
             XYZ Studio transcends the traditional digital agency; we're your
             cosmic companion in the digital odyssey.
           </p>
-          <p className="text-white text-2xl leading-36px mt-4 font-regular">
+          <p className="text-white mt-2  sm:mt-4 text-xs leading-5 sm:text-base  sm:leading-6  md:text-2xl md:leading-36px font-regular">
             We crave a captivating website, a high-flying mobile app,
             streamlined web applications, or a powerful digital marketing
             strategy, our expertise is poised to manifest your celestial visit
           </p>
           <div className="mt-6">
-            <Button width="w-180px" buttonType={'black'} title={'Learn More'} />
+            <Button
+              width="sm:w-[180px]"
+              height="sm:h-[56px]"
+              widthSmall="w-[94px]"
+              heightSmall="h-[32px]"
+              buttonType={'black'}
+              title={'Learn More'}
+              onPress={function (): void {
+                throw new Error('Function not implemented.');
+              }}
+            />
           </div>
         </div>
-        <div className="background-red z-10 mr-[76px]">
+        <div className="background-red hidden sm:block z-10 mr-[76px]">
           <Image src={ASTRONAUT_SITTING} alt="ASTRONAUT SITTING" />
         </div>
       </div>
-
       <BgCircles />
     </div>
   );
--- src/app/websitedevelopment/page.tsx
+++ src/app/websitedevelopment/page.tsx
@@ -1,5 +1,6 @@
+'use client';
 import Header from '@/components/atoms/header';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Intro from './components/Intro';
 import About from './components/about';
 import Info from './components/Info';
@@ -8,26 +9,32 @@ import Services from './components/services';
 import CustomModal from '@/components/molecules/modal';
 
 const Web = () => {
+  const divRef = useRef<HTMLDivElement | null>(null);
+  const scrollToComp = () => {
+    if (divRef.current) {
+      divRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <div>
       {/* <CustomModal /> */}
       <div className="w-full md:pl-20 pl-10 rounded-s-2xl bg-primary">
         <Header />
-        <div className=" flex justify-center flex-row md:mt-5">
-          <Intro type={'website'} />
-        </div>
+        {/* <div className="flex justify-center flex-row md:mt-5">
+          <Intro type={'website'} onPress={scrollToComp} />
+        </div> */}
       </div>
       <div className="w-full bg-white z-[-2] relative pt-124px">
         <div className="h-370px bg-primary absolute  z-[-1] top-0 left-0 w-full rounded-b-80px" />
         <About />
       </div>
-      <div className="pl-20 overflow-hidden bg-white pt-124px pb-72px">
+      {/* <div className="pl-20 bg-white pt-124px pb-72px">
         <Info />
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <Services />
-      </div>
-      <div className="my-124px mx-20">
+      </div> */}
+      <div ref={divRef} className="my-124px mx-20">
         <FormView />
       </div>
     </div>
